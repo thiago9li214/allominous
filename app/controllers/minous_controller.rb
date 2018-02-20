3beabb5b1b69a0515e8bd89c38d0c35ac4276b6f
Nathalie Roch <nathalie.m.roch@gmail.com>
2018-02-20 15:01:54 +0100

    controller minous

--- app/controllers/minous_controller.rb
+++ app/controllers/minous_controller.rb
@@ -1,14 +1,25 @@
 class MinousController < ApplicationController
+  before_action :set_minou, only: [:show, :edit]
+
+
   def index
+    @minous = Minou.all
   end
 
   def show
   end
 
   def new
-
+    @minou = Minou.new
   end
 
   def edit
   end
+
+  private
+
+  def set_minou
+    @minou = Minou.find(params[:id])
+  end
+
 end
