40998cfebdaa47c7a2f5810047425d8720de9eca
Tanguy Foujols <tanguy.foujols@icloud.com>
2018-02-20 17:28:38 +0100

    1) Formulaire rentals 2) Validation rentals 3) MAJ de la show des minous pour afficher les rentals du minou

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,28 +1,12 @@
 class RentalsController < ApplicationController
 
-  # before_action :set_minou, only: [:show, :edit, :destroy]
-
-
-  # def index
-  #   @minous = Minou.all
-  # end
-
-  # def show
-  #   @minou = Minou.find(params[:id])
-  #   @rental = Rental.new
-  # end
-
-  # def new
-  #   @minou = Minou.new
-  # end
-
   def create
     @rental = Rental.new(rental_params)
     # Add the corresponding user and minou to rental
     @rental.user = current_user
     minou = Minou.find(params[:minou_id])
     @rental.minou = minou
-    # Rental price calculus
+    # Rental price calculus a faire quand les date picker seront done
     # @rental.rental_price = minou.daily_price * ( params[:rental][:end_date] - params[:rentals][:begin_date] )
     if @rental.save
       redirect_to minou_path(minou)
@@ -31,19 +15,7 @@ class RentalsController < ApplicationController
     end
   end
 
-  # def edit
-  # end
-
-  # def destroy
-  #   @minou.destroy
-  #   redirect_to minous_path
-  # end
-
   private
-
-  # def set_minou
-  #   @minou = Minou.find(params[:id])
-  # end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def rental_params
