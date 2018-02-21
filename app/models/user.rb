597509b89a706108e431d900d3ef59af58f99703
Thomas CDF <tdesfrancs@gmail.com>
2018-02-21 13:47:22 +0100

    Minouboard => style and little back

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :minous
   has_many :rentals
+  # has_many :rented_minous, through: :rentals, source: :minou
   validates :first_name, presence: :true, allow_blank: :false
   validates :last_name, presence: :true, allow_blank: :false
   validates :address, presence: :true, allow_blank: :false
