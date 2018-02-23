6109b62654ace3f7d722a41a6693042661385526
Maximilien Barbe <maximilien.barbe.26@gmail.com>
2018-02-23 12:43:48 +0100

    seed modified PARIS AND LYON

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ user1.save!
 # User 2
 url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/g_face,c_thumb,w_200,h_200/v1519223922/e%CC%81pe%CC%81e_copie.jpg'
 user2 = User.new( {
-    first_name:  'MinouLover',
+    first_name:  'MinouAccro',
     last_name:   'KatFan',
     email:       'minous2@gmail.com',
     password: 'azerty',
@@ -40,8 +40,8 @@ user2.save!
 # User 3
 url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/g_face,c_thumb,w_200,h_200/v1519223922/contrebois_copie.jpg'
 user3 = User.new( {
-    first_name:  'MinouLover',
-    last_name:   'KatFan',
+    first_name:  'Mickey',
+    last_name:   'Minousse',
     email:       'minous3@gmail.com',
     password: 'azerty',
     address:      '27 avenue de la grande armée, Paris'
@@ -52,14 +52,125 @@ user3.save!
 # User 4
 url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/v1519226474/man-with-blanket.jpg'
 user4 = User.new( {
-    first_name:  'MinouLover',
-    last_name:   'KatFan',
+    first_name:  'Francois',
+    last_name:   'Hollande',
     email:       'minous4@gmail.com',
     password: 'azerty',
     address:      '2 rue des Princes, Boulogne-Billancourt'
   } )
 user4.remote_photo_url = url
 user4.save!
+
+url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/v1519226474/man-with-blanket.jpg'
+user5 = User.new( {
+    first_name:  'Marcel',
+    last_name:   'Pagnol',
+    email:       'minous5@gmail.com',
+    password: 'azerty',
+    address:      '32 Rue Saint-Marc, 75002 Paris'
+  } )
+user5.remote_photo_url = url
+user5.save!
+
+url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/v1519226474/man-with-blanket.jpg'
+user6 = User.new( {
+    first_name:  'Emmanuel',
+    last_name:   'Macron',
+    email:       'minous6@gmail.com',
+    password: 'azerty',
+    address:      '14 Rue Bonaparte, 75006 Paris'
+  } )
+user6.remote_photo_url = url
+user6.save!
+
+url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/v1519226474/man-with-blanket.jpg'
+user7 = User.new( {
+    first_name:  'Edison',
+    last_name:   'Cavani',
+    email:       'minous7@gmail.com',
+    password: 'azerty',
+    address:      '33 Rue Barbès, 92120 Montrouge'
+  } )
+user7.remote_photo_url = url
+user7.save!
+
+url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/v1519226474/man-with-blanket.jpg'
+user8 = User.new( {
+    first_name:  'Neymar',
+    last_name:   'Junior',
+    email:       'minous8@gmail.com',
+    password: 'azerty',
+    address:      '133 Rue de Turenne, 75003 Paris'
+  } )
+user8.remote_photo_url = url
+user8.save!
+
+url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/v1519226474/man-with-blanket.jpg'
+user9 = User.new( {
+    first_name:  'Kyllian',
+    last_name:   'Mbappe',
+    email:       'minous9@gmail.com',
+    password: 'azerty',
+    address:      '29 Rue Debelleyme, 75003 Paris'
+  } )
+user9.remote_photo_url = url
+user9.save!
+
+url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/v1519226474/man-with-blanket.jpg'
+user10 = User.new( {
+    first_name:  'Marco',
+    last_name:   'Verrati',
+    email:       'minous10@gmail.com',
+    password: 'azerty',
+    address:      '2 Rue Ambroise Paré, 75010 Paris'
+  } )
+user10.remote_photo_url = url
+user10.save!
+
+url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/v1519226474/man-with-blanket.jpg'
+user11 = User.new( {
+    first_name:  'Thiago',
+    last_name:   'Motta',
+    email:       'minous11@gmail.com',
+    password: 'azerty',
+    address:      '35 Rue du Chevalier de la Barre, 75018 Paris'
+  } )
+user11.remote_photo_url = url
+user11.save!
+
+url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/v1519226474/man-with-blanket.jpg'
+user12 = User.new( {
+    first_name:  'Nabil',
+    last_name:   'Fekir',
+    email:       'minous12@gmail.com',
+    password: 'azerty',
+    address:      'Place Bellecour, 69002 Lyon'
+  } )
+user12.remote_photo_url = url
+user12.save!
+
+
+url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/v1519226474/man-with-blanket.jpg'
+user13 = User.new( {
+    first_name:  'Anthony',
+    last_name:   'Lopes',
+    email:       'minous13@gmail.com',
+    password: 'azerty',
+    address:      '1 Rue Saint-Nizier, 69002 Lyon'
+  } )
+user13.remote_photo_url = url
+user13.save!
+
+url = 'http://res.cloudinary.com/dxjn2cmij/image/upload/v1519226474/man-with-blanket.jpg'
+user14 = User.new( {
+    first_name:  'Mariano',
+    last_name:   'Diaz',
+    email:       'minous14@gmail.com',
+    password: 'azerty',
+    address:      '108 Grande Rue de la Guillotière, 69007 Lyon'
+  } )
+user14.remote_photo_url = url
+user14.save!
 
 
 
@@ -206,7 +317,7 @@ Minou.create({
     personality:  'susceptible',
     description:  'Il botterait bien les fesses de Jerry.',
     daily_price:  8,
-    user_id:      user1.id
+    user_id:      user5.id
   })
 
 
@@ -218,7 +329,7 @@ Minou.create({
     personality:  'grumpy',
     description:  'Il botterait bien les fesses de Jerry.',
     daily_price:  8,
-    user_id:      user1.id
+    user_id:      user6.id
   })
 
 Minou.create({
@@ -229,7 +340,7 @@ Minou.create({
     personality:  'susceptible',
     description:  'Il botterait bien les fesses de Jerry.',
     daily_price:  8,
-    user_id:      user1.id
+    user_id:      user7.id
   })
 
 Minou.create({
@@ -240,7 +351,7 @@ Minou.create({
     personality:  'espiègle',
     description:  'Il botterait bien les fesses de Jerry.',
     daily_price:  8,
-    user_id:      user1.id
+    user_id:      user8.id
   })
 
 
@@ -252,7 +363,7 @@ Minou.create({
     personality:  'susceptible',
     description:  'Il botterait bien les fesses de Jerry.',
     daily_price:  8,
-    user_id:      user1.id
+    user_id:      user9.id
   })
 
 Minou.create({
@@ -263,7 +374,7 @@ Minou.create({
     personality:  'sournois',
     description:  'Il botterait bien les fesses de Jerry.',
     daily_price:  8,
-    user_id:      user1.id
+    user_id:      user10.id
   })
 
 Minou.create({
@@ -274,9 +385,41 @@ Minou.create({
     personality:  'calin',
     description:  'Il botterait bien les fesses de Jerry.',
     daily_price:  8,
-    user_id:      user1.id
+    user_id:      user11.id
   })
 
+Minou.create({
+    name:         'Sarkozy',
+    size:         'moyen',
+    specie:       'chat',
+    age:          6,
+    personality:  'calin',
+    description:  'Il botterait bien les fesses de Jerry.',
+    daily_price:  8,
+    user_id:      user12.id
+  })
+
+Minou.create({
+    name:         'Harlem',
+    size:         'moyen',
+    specie:       'chat',
+    age:          6,
+    personality:  'calin',
+    description:  'Il botterait bien les fesses de Jerry.',
+    daily_price:  8,
+    user_id:      user13.id
+  })
+
+Minou.create({
+    name:         'Churchill',
+    size:         'moyen',
+    specie:       'chat',
+    age:          6,
+    personality:  'calin',
+    description:  'Il botterait bien les fesses de Jerry.',
+    daily_price:  8,
+    user_id:      user14.id
+  })
 
 
 
